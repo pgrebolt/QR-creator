e646971dd3b48c05ff91c030964b929df60e3797
pgrebolt <paugrebol@gmail.com>
2025-01-22 17:43:42 +0100

    Commented code

--- qr_creator.py
+++ qr_creator.py
@@ -1,13 +1,21 @@
 # Basic QR code creator using Segno library
 
+# Import Segno library
 import segno
 
+# Define file or URL to encode
 url = "https://onlinelibrary.wiley.com/doi/full/10.1002/smsc.202400066"
-red_color = '#d11d33'
 
+# Define saving name
+saving_name = "qr_article.png"
+
+red_color = '#d11d33' # (color vermell correbars)
+
+# Create the QR code
 qrcode = segno.make_qr(url)
 
-qrcode.save("qr_article.png",
+# Save the QR code with the desired properties
+qrcode.save(saving_name,
             scale = 5,
             border = 2,
             dark = '#4472C4',
